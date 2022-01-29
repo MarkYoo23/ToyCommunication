569b46a71169360cb79d9efe81218a682668f661
rd-dhyoo <71996956+rd-dhyoo@users.noreply.github.com>
2022-01-29 20:54:10 +0900

    Fixed Response Packet

--- ToyCommunication.Domain/Packets/Interface/IResponsePacket.cs
+++ ToyCommunication.Domain/Packets/Interface/IResponsePacket.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToyCommunication.Domain.Packets
+﻿namespace ToyCommunication.Domain.Packets
 {
     public interface IResponsePacket : ICommandPacket
     {
-
+        void Append(string message);
     }
 }
